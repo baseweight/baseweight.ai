9322f75490f928d86ac9ae14a0edcf8224c76b8e
Joe Bowser <bowserj@gmail.com>
2024-08-15 00:29:45 -0700

    Adding hacked files from infil00p.org

---
+++ components/footer.tsx
@@ -0,0 +1,25 @@
+import Container from './container'
+import Image from 'next/image'
+import { EXAMPLE_PATH } from '../lib/constants'
+
+const Footer = () => {
+  return (
+    <footer className="bg-neutral-50 border-t border-neutral-200">
+      <Container>
+        <div className='py-10 grid gap-2 grid-cols-2 grid-rows-6'>
+          <div><a href="/">Home</a></div>
+          <div>Foo</div>
+          <div><a href="/about">About</a></div>
+          <div>Bar</div>
+          <div><a href="/posts/speaking">Speaking</a></div>
+          <div>Baz</div>
+          <div></div>
+          <div></div>
+          <div>Copyright 2024 Baseweight Solutions Inc</div>
+        </div>
+      </Container>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ components/splash.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+
+const Splash = () => {
+    return (
+    <section>
+      <div className="mb-8 md:mb-16">
+        Stuff
+      </div>      
+    </section>
+    )
+}
+
+export default Splash
